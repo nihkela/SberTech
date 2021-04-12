c36ae8e0f8aa007ee22e4f6fb362fee082194334
Alexey Alekhin <nihkela@gmail.com>
2021-04-12 11:57:15 +0300

    Add main thread

--- src/homework02/Test.java
+++ src/homework02/Test.java
@@ -6,10 +6,36 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
         List<Car> cars = new ArrayList<>();
+        List<Car> sedans = new ArrayList<>();
+        List<Car> hatchbacks = new ArrayList<>();
+        List<Car> crossovers = new ArrayList<>();
+        List<Car> otherTypes = new ArrayList<>();
+
         cars.add(new Car("Lada", "Седан"));
         cars.add(new Car("Lada", "Хэтчбэк"));
         cars.add(new Car("Mercedes", "Седан"));
         cars.add(new Car("BMW", "Кроссовер"));
-        cars.add(new Car("Lada", "Sedan"));
+        cars.add(new Car("Peugeot", "Кроссовер"));
+        cars.add(new Car("Ford", "Хэтчбэк"));
+        cars.add(new Car("Toyota", "Седан"));
+        cars.add(new Car("Mitsubishi", "Универсал"));
+
+
+        for (Car car : cars) {
+            if (car.getType().equals("Седан")) {
+                sedans.add(car);
+            } else if (car.getType().equals("Хэтчбэк")) {
+                hatchbacks.add(car);
+            } else if (car.getType().equals("Кроссовер")) {
+                crossovers.add(car);
+            } else {
+                otherTypes.add(car);
+            }
+        }
+
+        System.out.println("Список седанов: " + sedans);
+        System.out.println("Список хэтчбэков: " + hatchbacks);
+        System.out.println("Список кроссоверов: " + crossovers);
+        System.out.println("Список машин других типов: " + otherTypes);
     }
 }
